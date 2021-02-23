84805f05d6caedcb34aa1bc627405240f9c75f70
Paulo Cesar Hernandez Rosado <paulocesarhero@gmail.com>
2021-02-23 13:38:40 -0600

    ejemplos de como hacer cadenas en java

---
+++ Cadena.java
@@ -0,0 +1,12 @@
+public class Cadena {
+    String nombreVariable = "";
+    //como hacer arreglos
+    int nombre[] = new int[4];
+    nombre[0]= new int[1];
+    public Cadena(){//constructos por default
+        //cuerpo constructor
+    }
+    public Cadena(String k, double j, int u){
+        //cuerpo de constructor
+    }
+}
