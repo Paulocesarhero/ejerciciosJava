092ddfc55ef68794962d7e91a46d97d342423176
Paulo Cesar Hernandez Rosado <paulocesarhero@gmail.com>
2021-02-24 14:11:33 -0600

    este es un programa que enseña string.value

---
+++ TipoCadena.java
@@ -0,0 +1,16 @@
+public class TipoCadena {
+    public static void main (String[] args){
+        int k = -12;
+        char c = 'F';
+        long kl =9999;
+        boolean sw = false;
+        float f = 25.50f;
+        double d = -15.45;
+        System.out.println("Entero k: "+ String.valueOf(k));
+        System.out.println("char c:" + String.valueOf(c));
+        System.out.println("long kl:" + String.valueOf(kl));
+        System.out.println("bool sw:" + String.valueOf(sw));
+        System.out.println("float f:" + String.valueOf(f));
+        System.out.println("double d" + String.valueOf(d));
+    }
+}
