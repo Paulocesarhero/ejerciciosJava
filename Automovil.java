c984c8337f6da165440dc2f5f18c04a7fd12aaec
Paulo Cesar Hernandez Rosado <paulocesarhero@gmail.com>
2021-03-06 12:15:05 -0600

    Mis programas de Herencia

---
+++ Automovil.java
@@ -0,0 +1,35 @@
+public class Automovil {
+    private String color;
+    private String modelo;
+    private int anio;
+    //metodos get
+
+    public String getColor() {
+        return this.color;
+    }
+
+    public String getModelo() {
+        return this.modelo;
+    }
+
+    public int getAnio() {
+        return this.anio;
+    }
+
+    //metodos set
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setAnio(int anio) {
+        this.anio = anio;
+    }
+    public static void main (String [] arg){
+     
+    }
+}
